3f52bc84ed318697e7e737feaa826d16224e65e3
coby5502 <coby5502@gmail.com>
2026-01-10 00:51:05 +0900

    Initial Commit

---
+++ utouto/utoutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  utoutoApp.swift
+//  utouto
+//
+//  Created by Doyoung Kim on 1/10/26.
+//
+
+import SwiftUI
+
+@main
+struct utoutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
